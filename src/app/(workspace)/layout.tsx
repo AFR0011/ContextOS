fe0aad9228d7e325372f613516bcc54ae634955f
Ali Farrokhnejad <farrokh445@gmail.com>
2026-08-12 22:41:42 +0300

    feat: scope workspace provider to authenticated user

--- src/app/(workspace)/layout.tsx
+++ src/app/(workspace)/layout.tsx
@@ -5,7 +5,7 @@ import WorkspaceShell from "@/components/workspace/WorkspaceShell";
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   const user = await requireUser();
   return (
-    <WorkspaceProvider>
+    <WorkspaceProvider user={user}>
       <WorkspaceShell user={user}>{children}</WorkspaceShell>
     </WorkspaceProvider>
   );
